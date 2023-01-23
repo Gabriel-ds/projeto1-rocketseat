dd6e3a9dc032b33b83774b91c041dc1d8dd1d5b2
Gabriel dos Santos <gabrields1910@gmail.com>
2023-01-23 17:33:14 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Header } from "./components/Header";
+
+import "./styles.css";
+import "./global.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>Helooo</h1>
+    </>
+  );
+}
+
+export default App;
